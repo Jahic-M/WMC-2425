a84e4ee92bdf2ace8ba2402031a276eaad59da39
Jahic-M <145590012+Jahic-M@users.noreply.github.com>
2026-01-29 20:51:32 +0100

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,13 @@
+// Objekte
+let person = {
+    name: "Max",
+    age: 18,
+    city: "Linz"
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+
+person.age = 19;
+console.log(person);
