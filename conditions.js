066d0d5069f8f7080de4b621c69800d50f480e97
Jahic-M <145590012+Jahic-M@users.noreply.github.com>
2026-01-29 20:49:33 +0100

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,16 @@
+// Bedingungen mit if / else
+let number = 7;
+
+if (number % 2 === 0) {
+    console.log("Die Zahl ist gerade");
+} else {
+    console.log("Die Zahl ist ungerade");
+}
+
+let age = 17;
+
+if (age >= 18) {
+    console.log("Volljährig");
+} else {
+    console.log("Minderjährig");
+}
