65f9069f954f9a2b99b6d68f7dc471194e98f1ad
Jahic-M <145590012+Jahic-M@users.noreply.github.com>
2026-01-29 20:51:51 +0100

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,16 @@
+// Variablen und einfache Ausgaben
+let name = "Max";
+let age = 18;
+let city = "Linz";
+
+console.log(name);
+console.log(age);
+console.log(city);
+
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
