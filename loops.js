a5be7dd4ea2b043480e0884c8375461ca0cdb864
Jahic-M <145590012+Jahic-M@users.noreply.github.com>
2026-01-29 20:51:09 +0100

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,10 @@
+// Schleifen
+for (let i = 1; i <= 5; i++) {
+    console.log("Zahl: " + i);
+}
+
+let counter = 0;
+while (counter < 3) {
+    console.log("Counter: " + counter);
+    counter++;
+}
